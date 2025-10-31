51f50b6f9cffbf86fc851e4c0849752a6d250645
Suzanne Pons <suzanne.pons68@gmail.comgit config --global user.email suzanne.pons68@gmail.com>
2025-10-31 17:23:29 +0100

    avancée sur la fonction ajouter personne avec une condition if..elseif...else pour id actor et id director

--- controller/PersonController.php
+++ controller/PersonController.php
@@ -60,7 +60,7 @@ class PersonController {
         //ALORS elles sont filtré pour s'assurer que la variable soit pas faussée (malveillance, faute de frappe,...) avec filter_input
         $firstName = filter_input(INPUT_POST, "first_name", FILTER_SANITIZE_FULL_SPECIAL_CHARS); //filter sanitize permet de "nettoyer" les données en retirant les balises html et d'encoder les charactères qui sont en dehors des normes ASCII (full special chars)
         $lastName = filter_input(INPUT_POST, "last_name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $birthday = filter_input(INPUT_POST, "birthday", FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_VALIDATE_INT);
+        $birthday = filter_input(INPUT_POST, "birthday", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $nationality = filter_input(INPUT_POST, "nationality", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $sexe = filter_input(INPUT_POST, "sexe", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $filmography = filter_input(INPUT_POST, "filmography", FILTER_VALIDATE_INT);
@@ -69,24 +69,38 @@ class PersonController {
         $director = isset($_POST['director']) ? 1 : 0;
 
             if($firstName && $lastName && $birthday && $nationality && $sexe && $filmography){  //SI on a la variable filtré
-                $requeteAddPerson = $pdo->prepare("INSERT INTO person p (p.first_name, p.last_name, p.birthday, p.nationality, p.sexe, p.filmography)
+                $requeteAddPerson = $pdo->prepare("INSERT INTO person (first_name, last_name, birthday, nationality, sexe, filmography)
                 VALUES (:first_name, :last_name, :birthday, :nationality, :sexe, :filmography)");
                 $requeteAddPerson->execute(['first_name' => $first_name], ['last_name' => $last_name], ['birthday' => $birthday], ['nationality' => $nationality], ['sexe' => $sexe], ['filmography' => $filmography]);
-                header("Location: index.php?action=listActeurs"); //On veut éviter la répétition au rafraîchissement de l'action
-                exit;
-            }
-        
-            if(!$actor || !$director) { //Si actor OU director n'est pas coché
-                echo "Veuillez cocher au moins l'une des deux cases";
+            };
 
-            } elseif ($actor && $director) {
-                $idPerson = $pdo->lastInsertedID();
-                $requeteAddID = $pdo->prepare("INSERT INTO actor (id_person)
-                VALUES (:id);
-                INSERT INTO director (id_person)
+            if(!$actor && !$director) { //Si actor ET director n'est pas coché
+                echo "Veuillez cocher au moins l'une des deux cases"; //ALORS on affiche un message d'erreur
+                return;
+            };
+
+            if($actor && $director) { //Si actor OU director n'est pas coché
+                $idPerson = $pdo->lastInsertedId();
+                $requeteAddIdActor = $pdo->prepare("INSERT INTO actor (id_person)
                 VALUES (:id)");
+                $requeteAddIdActor->execute(["id" => $idPerson]);
+                $requeteAddID = $pdo->prepare("INSERT INTO director (id_person)
+                VALUES (:id)");
+                $requeteAddIdDirector->execute(["id" => $idPerson]);
+                require "view/accueil.php";
+
+            } elseif ($actor) {
+                $idPerson = $pdo->lastInsertedI();
+                $requeteAddID = $pdo->prepare("INSERT INTO actor (id_person) VALUES (:id);");
                 $requeteAddID->execute(["id" => $idPerson]);
-            } else 
+                require "view/person/listActeurs.php";
+
+            } else {
+                $idPerson = $pdo->lastInsertedId();
+                $requeteAddID = $pdo->prepare("INSERT INTO director (id_person) VALUES (:id);");
+                $requeteAddID->execute(["id" => $idPerson]);  
+                require "view/person/listRealisateurs.php";  
+            }
 
         }
     }
